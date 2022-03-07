02019efb2aa1c992a4b787a19e23398c7bc83328
jovisaib <jovisaib@gmail.com>
2022-03-07 19:16:34 +0100

    wip

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, division, unicode_literals
+# from __future__ import print_function, division, unicode_literals
 # import json
 
 from flask import Flask,request, render_template, jsonify
